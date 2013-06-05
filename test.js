5464a301f41e8cc8a2b836b0c07b5d93e72193ae
Raine Virta <raine.virta@gmail.com>
2013-06-05 22:29:26 +0300

    update test.js

--- test.js
+++ test.js
@@ -39,3 +39,27 @@ assert(!handleChinese('zhu2', 'zhu2'));
 localStorage['forgive-typos-pinyin-disable'] = true;
 
 assert(handleChinese('zhu2', 'zhu2'));
+
+var handleFrench = function(given, correct) {
+	var ARTICLES = [ 'le', 'la', 'les', 'un', 'une', 'des' ];
+	var regex    = new RegExp('\\b(' + ARTICLES.join('|') + ')\\b', 'gi');
+	var article  = function(str) {
+		var m = str.match(regex);
+		return m && m[0];
+	};
+
+	var givenArticle   = article(given);
+	var correctArticle = article(correct);
+	if (correctArticle && correctArticle !== givenArticle) {
+		return true;
+	}
+
+	return false;
+};
+
+assert(handleFrench('un oiseau', 'une oiseau'));
+
+assert(!handleFrench('une oiseau', 'une oiseau'));
+assert(!handleFrench('une oiseai', 'une oiseau'));
+assert(!handleFrench('uneoiseau', 'uneoiseau'));
+assert(!handleFrench('uneoiseau', 'uneoiseau'));
